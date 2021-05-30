ba964c590ac033695d3e3f381ca4bfa1502f4270
SO <SO@SO-MacBook-Air.local>
2021-05-30 22:07:50 +0900

    babel 적용

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-const express = require("express");
+// const express = require("express");
+// this is more sexy code...^^
+import express from "express";
 
 const app = express();
+
+console.log("Hi!!")
